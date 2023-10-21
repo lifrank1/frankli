54b57126ebdcd4de881b19f92866b0ef38c529e8
lifrank1 <frank.li.865985@gmail.com>
2023-10-21 00:57:09 -0400

    react app setup with vite

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+// Import component before using it
+import Message from './Message';
+
+function App() {
+  return <div><Message/></div>
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,14 @@
+
+// PascalCasing <- used for creating a function component. capitalize every word
+function Message() {
+
+    const name = 'Most';
+    // JSX : JavaScript XML (compiled to javascript through babel)
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello</h1>
+}
+
+// To use this:
+// Go to app.tsx and 
+export default Message;
