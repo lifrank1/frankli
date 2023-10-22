159dd5e308929904158c3c1e048c1ee938b9acd6
lifrank1 <frank.li.865985@gmail.com>
2023-10-22 15:09:10 -0400

    data cleanup

--- data/ExperienceData.tsx
+++ data/ExperienceData.tsx
@@ -1,28 +1,26 @@
-
-
-export const ExperienceData  = [
-    {
-        company: "Fiserv",
-        link: "https://www.fiserv.com/en.html",
-        role: "Software Engineer Intern",
-        location: "Columbus, OH",
-        time: "June 2023 - August 2023",
-        achievements: ["Worked on an agile team"]
-    },
-    {
-        company: "ExstoBio",
-        link: "https://www.fiserv.com/en.html",
-        role: "Innovation Intern",
-        location: "Columbus, OH",
-        time: "June 2023 - August 2023",
-        achievements: ["CSE 2221"]
-    },
-    {
-        company: "OSU Department of CS",
-        link: "https://www.fiserv.com/en.html",
-        role: "Teaching Assistant",
-        location: "Columbus, OH",
-        time: "June 2023 - August 2023",
-        achievements: ["CSE 2221"]
-    }
-] 
+export const ExperienceData = [
+  {
+    company: "Fiserv",
+    link: "https://www.fiserv.com/en.html",
+    role: "Software Engineer Intern",
+    location: "Columbus, OH",
+    time: "June 2023 - August 2023",
+    achievements: ["Worked on an agile team"],
+  },
+  {
+    company: "ExstoBio",
+    link: "https://www.fiserv.com/en.html",
+    role: "Innovation Intern",
+    location: "Columbus, OH",
+    time: "June 2023 - August 2023",
+    achievements: ["Research"],
+  },
+  {
+    company: "OSU Department of CS",
+    link: "https://www.fiserv.com/en.html",
+    role: "Teaching Assistant/Student Researcher",
+    location: "Columbus, OH",
+    time: "June 2023 - August 2023",
+    achievements: ["CSE 2221"],
+  },
+];
--- data/LeadershipData.tsx
+++ data/LeadershipData.tsx
@@ -1,16 +1,16 @@
 export const LeadershipData = [
   {
     company: "Big Data Analytics Association",
-    link: "https://www.fiserv.com/en.html",
-    role: "Software Engineer Intern",
+    link: "https://bdaaosu.org/",
+    role: "Special Project Coordinator",
     location: "Columbus, OH",
     time: "June 2023 - August 2023",
     achievements: ["Worked on an agile team"],
   },
   {
-    company: "ExstoBio",
-    link: "https://www.fiserv.com/en.html",
-    role: "Innovation Intern",
+    company: "Dublin Tennis Outreach Program",
+    link: "https://www.dublintop.org/",
+    role: "Director - Dublin",
     location: "Columbus, OH",
     time: "June 2023 - August 2023",
     achievements: ["CSE 2221"],
