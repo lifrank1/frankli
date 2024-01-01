f239b9d227c3af5430313c4ffe27df054bc74c56
lifrank1 <frank.li.865985@gmail.com>
2024-01-01 18:09:30 -0500

    react local image handling

--- components/MeBlock.tsx
+++ components/MeBlock.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "../src/App.css";
+import portrait from "../data/headshot.jpeg";
 
 function MeBlock() {
   return (
     <div className="pic-and-name">
-      <img src={"../data/headshot.jpeg"} className="picture" />
+      <img src={portrait} className="picture" />
       <h1 className="name">Frank Li</h1>
     </div>
   );
