36bec55b05544f377a407fb65442b9ac8db15cf7
lifrank1 <frank.li.865985@gmail.com>
2024-01-01 18:05:57 -0500

    picture cors error

--- components/MeBlock.tsx
+++ components/MeBlock.tsx
@@ -4,11 +4,10 @@ import "../src/App.css";
 function MeBlock() {
   return (
     <div className="pic-and-name">
-      <img src="../data/headshot.jpeg" className="picture" />
+      <img src={"../data/headshot.jpeg"} className="picture" />
       <h1 className="name">Frank Li</h1>
     </div>
   );
 }
 
 export default MeBlock;
-
